9c1114fb4ddc8a72ff910ef1beb70b578658811f
Woojin Byun <raindoor38@gmail.com>
2017-05-24 17:36:31 +0900

    extract_decriptor 이

--- routes/user/register.js
+++ routes/user/register.js
@@ -85,9 +85,9 @@ function getDescriptor(filePath, fileName){
   afterImgs = afterImgs.filter(function(e) {
     return beforeImgs.indexOf(e) < 0;
   });
-  shell.cd('../../../caffe/build/extract_vector/');
+  shell.cd('../../../caffe/build/extract_decriptor/');
   for(var i=0; i<afterImgs.length; i++){
-    shell.exec('./extract_vector ' + filePath + ' ' + afterImgs[i]);
+    shell.exec('./extract_decriptor ' + filePath + ' ' + afterImgs[i]);
   }
 
 
