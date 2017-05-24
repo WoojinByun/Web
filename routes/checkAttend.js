4a222a8221fa498d4c929dfd0a2f578b0354b856
Woojin Byun <raindoor38@gmail.com>
2017-05-24 22:40:47 +0900

    dir 수

--- routes/checkAttend.js
+++ routes/checkAttend.js
@@ -83,8 +83,9 @@ function getDescriptor(filePath, fileName){
   for(var i=0; i<afterImgs.length; i++){
     console.log('-----------------' + './extract_descriptor ' + userDir + ' ' + afterImgs[i]);
     shell.exec('./extract_descriptor ' + userDir + ' ' + afterImgs[i]);
+    shell.cd(rootDir + '/../face_recognition/src/build/');
     console.log('-----------------' + './check_attendance ' + userDir + afterImgs[i].replace('.png','.txt') + ' 34 35 37');
-    var file = shell.exec('./check_attendance ' + userDir + afterImgs[i].replace('.png','.txt') + ' 34 35 37');
+    var file = shell.exec('./check_attendance ' + userDir + afterImgs[i].replace('.png','.txt') + ' 34 35 37').stdout;
     files.push(file);
   }
   console.log('---------------------=-=-=-=---=-=-=-=---=-=-=-=---=-=-=-=---=-=-=-=---=-=-=-=---=-=-=-=---=-=-=-=--');
