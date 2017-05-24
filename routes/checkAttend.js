44ff7d343153a47a5d4a4b37400863e70e1c630b
Woojin Byun <raindoor38@gmail.com>
2017-05-24 22:52:26 +0900

    무슨문제지

--- routes/checkAttend.js
+++ routes/checkAttend.js
@@ -68,8 +68,8 @@ function display(req, res){
 }
 
 function getDescriptor(filePath, fileName){
-  shell.exec('rm -rf ' + rootDir + '/public/attChk/*.png');
-  shell.exec('rm -rf ' + rootDir + '/public/attChk/*.txt');
+  shell.exec('rm ' + rootDir + '/public/attChk/*.png');
+  shell.exec('rm ' + rootDir + '/public/attChk/*.txt');
   var userDir = filePath.replace('/temp','')
   var beforeImgs = shell.ls(userDir + '*.*g').stdout.split('\n');
   shell.cd(rootDir + '/../face_recognition/src/build/');
