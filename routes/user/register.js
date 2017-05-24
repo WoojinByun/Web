54de96cf8d8cd67fd2052730322e6f45c19479bd
Woojin Byun <raindoor38@gmail.com>
2017-05-24 19:15:29 +0900

    경로 오류 수

--- routes/user/register.js
+++ routes/user/register.js
@@ -68,7 +68,7 @@ function display(req, res){
 function getDescriptor(filePath, fileName){
   var userDir = filePath.replace('/temp','')
   var beforeImgs = shell.ls(userDir + '*.*g').stdout.split('\n');
-  shell.cd('../../face_recognition/src/build/');
+  shell.cd('../../../face_recognition/src/build/');
   shell.exec('./crop ' + userDir + ' ' + filePath+fileName);
   shell.rm(filePath+fileName);
   shell.cd(userDir);
