31dbdbbbb23045aa04b92a36cfc0a497e7953070
Woojin Byun <raindoor38@gmail.com>
2017-06-07 16:42:35 +0900

    image refresh test

---
+++ public/js/classAttend.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+});
+
+$('.imgCover').click(function(){
+  d = new Date();
+  var imgSrc = $('img', this).attr('src').split('?')[0];
+  $('img', this).attr('src', imgSrc + "?" +d.getTime());
+  alert("!");
+});
