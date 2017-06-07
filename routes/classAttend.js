698b6b1eadbd54a8f1be9c305f02a6f8297980bc
Woojin Byun <raindoor38@gmail.com>
2017-06-08 07:50:43 +0900

    string split

--- routes/classAttend.js
+++ routes/classAttend.js
@@ -48,65 +48,52 @@ router.use(function(req, res, next){
 router.post('/', function(req, res, next) {
   var datas = {};
   var form = new formidable.IncomingForm();
-  console.log("what the fuck");
-  console.log(req);
-  console.log(req.body);
-  console.log(req.body.timeData.couNum);
-  console.log(req.body.timeData.order);
-  console.log(req.body.timeData.time);
   form.parse(req, function(err, fields, files) {
     if (err) {
       console.error(err);
     }
-    console.log("!!!!!!!!!!!!!!!!!!!!!!");
     console.log(fields);
-  });
+    datas = fields.timeData.split('/');
+    var imgs = getDescriptor(rootDir + '/public/rasp/', shell.ls('public/rasp/attTest.*g').stdout.replace('public/rasp/','').split('\n')[0]);
+    var isNoPerson = false;
+    if(imgs == undefined){
+      res.redirect('/classAttend?msg='+'얼굴 검출에 실패하였습니다.');
+      return;
+    }
+    imgs.sort(function compareNumbers(a, b) {return parseInt(a.usrNum) - parseInt(b.usrNum);});
+    var usrNums = [];
+    console.log(imgs);
+    console.log(datas);
+    for(var j=0; j<imgs.length; j++){
+      usrNums.push(imgs[j].usrNum);
+    }
+    datas.usrNums = usrNums;
+    console.log(datas);
+    dbmodule.doAttend(datas);
 
-  form.on('end', function(fields, files) {
-    console.log("END!");
-    console.log(fields);
-    console.log(files);
+    var userEvt = dbmodule.getUsersInfo(datas.usrNums);
+    userEvt.on('end', function(error, users){
+      if (error) {
+        console.log(error);
+        res.writeHead(500);
+        res.end();
+      }
+      for(var j=0; j<users.length; j++){
+        users[j].imgSrc = imgs[j].imgSrc;
+      }
+      params.users = users;
+      var attendTimeEvent = dbmodule.getAttendTimeAll(params.user.usrNum);
+      attendTimeEvent.on('end', function(error, timeDatas){
+        if (error) {
+          console.log(error);
+          res.writeHead(500);
+          res.end();
+        }
+        params.timeDatas = timeDatas;
+        display(req, res);
+      });
+    });
   });
-  // datas = fields;
-  // var imgs = getDescriptor(rootDir + '/public/rasp/', shell.ls('public/rasp/attTest.*g').stdout.replace('public/rasp/','').split('\n')[0]);
-  // var isNoPerson = false;
-  // if(imgs == undefined){
-  //   res.redirect('/classAttend?msg='+'얼굴 검출에 실패하였습니다.');
-  //   return;
-  // }
-  // imgs.sort(function compareNumbers(a, b) {return parseInt(a.usrNum) - parseInt(b.usrNum);});
-  // var usrNums = [];
-  // console.log(imgs);
-  // console.log(datas);
-  // for(var j=0; j<imgs.length; j++){
-  //   usrNums.push(imgs[j].usrNum);
-  // }
-  // datas.usrNums = usrNums;
-  // console.log(datas);
-  // dbmodule.doAttend(datas);
-  //
-  // var userEvt = dbmodule.getUsersInfo(datas.usrNums);
-  // userEvt.on('end', function(error, users){
-  //   if (error) {
-  //     console.log(error);
-  //     res.writeHead(500);
-  //     res.end();
-  //   }
-  //   for(var j=0; j<users.length; j++){
-  //     users[j].imgSrc = imgs[j].imgSrc;
-  //   }
-  //   params.users = users;
-  //   var attendTimeEvent = dbmodule.getAttendTimeAll(params.user.usrNum);
-  //   attendTimeEvent.on('end', function(error, timeDatas){
-  //     if (error) {
-  //       console.log(error);
-  //       res.writeHead(500);
-  //       res.end();
-  //     }
-  //     params.timeDatas = timeDatas;
-  //     display(req, res);
-  //   });
-  // });
 });
 
 router.get('/', function(req, res, next) {
