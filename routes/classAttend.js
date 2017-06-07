f2cbda5dcdca0a3eb53872e745f0c5c4efd1aaf1
Woojin Byun <raindoor38@gmail.com>
2017-06-08 02:31:03 +0900

    잘못된 함수 수정

--- routes/classAttend.js
+++ routes/classAttend.js
@@ -47,8 +47,8 @@ router.use(function(req, res, next){
 
 router.post('/', function(req, res, next) {
   var datas = req.body.timeData;
-  console.log(rootDir + '/public/rasp/', shell.ls('public/rasp/attTest.*g').replace('public/rasp/','').stdout.split('\n')[0]);
-  var imgs = getDescriptor(rootDir + '/public/rasp/', shell.ls('public/rasp/attTest.*g').replace('public/rasp/','').stdout.split('\n')[0]);
+  console.log(rootDir + '/public/rasp/', shell.ls('public/rasp/attTest.*g').stdout.replace('public/rasp/','').split('\n')[0]);
+  var imgs = getDescriptor(rootDir + '/public/rasp/', shell.ls('public/rasp/attTest.*g').stdout.replace('public/rasp/','').split('\n')[0]);
   var isNoPerson = false;
   if(imgs == undefined){
     res.redirect('/classAttend?msg='+'얼굴 검출에 실패하였습니다.');
