37a260bf813b0a0faf468608410454db4bf2ab82
Woojin Byun <raindoor38@gmail.com>
2017-06-08 14:46:02 +0900

    test

--- public/js/classAttend.js
+++ public/js/classAttend.js
@@ -4,5 +4,5 @@ $(document).ready(function(){
 $('.imgCover').click(function(){
   d = new Date();
   var imgSrc = $('img', this).attr('src').split('?')[0];
-  $('img', this).attr('src', imgSrc + "?" + d.getTime());
+  $('img', this).attr('src', "/rasp/attTest.jpg?" + d.getTime());
 });
