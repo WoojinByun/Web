131552cf7fb2409e700197695c11dec8853b5127
Woojin Byun <raindoor38@gmail.com>
2017-06-14 01:07:13 +0900

    chart 반응형 width, mobile icon 수정

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 
 function makeDonutChart(target, data){
-  var svg = d3.select("svg"),
+  var svg = d3.selectAll("svg"),
       margin = {top: 20, right: 60, bottom: 30, left: 40},
       width = +svg.attr("width") - margin.left - margin.right,
       height = +svg.attr("height") - margin.top - margin.bottom,
@@ -32,23 +32,25 @@ function makeDonutChart(target, data){
   serie.selectAll("rect")
     .data(function(d) { return d; })
     .enter().append("rect")
-      .attr("x", function(d) { return x(d.data.couName); })
+      .attr("x", function(d) { return x.bandwidth()/4 + x(d.data.couName); })
       .attr("y", function(d) { return y(d[1]); })
       .attr("height", function(d) { return y(d[0]) - y(d[1]); })
-      .attr("width", x.bandwidth());
+      .attr("width", x.bandwidth()/2);
 
   g.append("g")
       .attr("class", "axis axis--x")
       .attr("transform", "translate(0," + height + ")")
+      .style("font", "16px sans-serif")
       .call(d3.axisBottom(x));
 
   g.append("g")
       .attr("class", "axis axis--y")
+      .style("font", "12px sans-serif")
       .call(d3.axisLeft(y).ticks(10, "%"));
 
   var legend = serie.append("g")
       .attr("class", "legend")
-      .attr("transform", function(d) { var d = d[d.length - 1]; return "translate(" + (x(d.data.couName) + x.bandwidth()) + "," + ((y(d[0]) + y(d[1])) / 2) + ")"; });
+      .attr("transform", function(d) { var d = d[d.length - 1]; return "translate(" + (x(d.data.couName) + x.bandwidth()*3/4) + "," + ((y(d[0]) + y(d[1])) / 2) + ")"; });
 
   legend.append("line")
       .attr("x1", -6)
@@ -59,7 +61,7 @@ function makeDonutChart(target, data){
       .attr("x", 9)
       .attr("dy", "0.35em")
       .attr("fill", "#000")
-      .style("font", "10px sans-serif")
+      .style("font", "16px sans-serif")
       .text(function(d) { return d.key; });
 
   function type(d, i, columns) {
